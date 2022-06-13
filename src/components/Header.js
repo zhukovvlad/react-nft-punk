1e4daa9e3ce4039c6de46cc634bca3029becede1
Zhukovvlad <zhukovvlad@yandex.ru>
2022-06-13 23:40:36 +0300

    Styled NavBar Component Search Input

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,23 @@ import React from 'react';
 import './Header.css';
 
 import punkLogo from '../assets/header/cryptopunk-logo.png';
+import searchIcon from '../assets/header/search.png';
 
 const Header = () => {
   return (
     <div className='header'>
         <div className='logoContainer'>
             <img src={punkLogo} className='punkLogo' alt='' />
+        </div>
+
+        <div className='searchBar'>
+            <div className='searchIconContainer'>
+                <img src={searchIcon} alt='' />
+            </div>
+            <input
+                className='searchInput'
+                placeholder='Collection, item or user...'
+            />
         </div>
     </div>
   )
