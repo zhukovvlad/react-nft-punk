44d4bb7416a8efa92071e00e048f7d14f2292b0f
Zhukovvlad <zhukovvlad@yandex.ru>
2022-06-14 23:18:55 +0300

    Started CollectionCard Components. Added some styles to the card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 
 import Header from './components/Header';
+import CollectionCard from './components/CollectionCard';
+
+import ExamplePunk from './assets/owner/5.jpg'
 
 function App() {
   return (
     <div className='app'>
       <Header />
+      <CollectionCard id={0} name={'Bandana Punk'} traits={[{'value': 7}]} image={ExamplePunk} />
     </div>
     )
 }
