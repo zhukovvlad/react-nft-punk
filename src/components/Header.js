4dfe81823db224be897e8f74594e630b08353813
Zhukovvlad <zhukovvlad@yandex.ru>
2022-06-14 22:35:40 +0300

    Finished styling Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import './Header.css';
 
 import punkLogo from '../assets/header/cryptopunk-logo.png';
 import searchIcon from '../assets/header/search.png';
+import themeSwitchIcon from '../assets/header/theme-switch.png';
 
 const Header = () => {
   return (
@@ -19,6 +20,22 @@ const Header = () => {
                 className='searchInput'
                 placeholder='Collection, item or user...'
             />
+        </div>
+
+        <div className='headerItems'>
+            <p>Drops</p>
+            <p>Marketplace</p>
+            <p>Create</p>
+        </div>
+
+        <div className='headerActions'>
+            <div className='themeSwitchContainer'>
+                <img src={themeSwitchIcon} alt='' />
+            </div>
+        </div>
+
+        <div className='loginButton'>
+            GET IN
         </div>
     </div>
   )
