b6279649eba7d290ebaa2f10ed58da13b521ea44
Zhukovvlad <zhukovvlad@yandex.ru>
2022-06-15 15:51:16 +0300

    Created PunkList Componenet and styled it

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import Header from './components/Header';
-import CollectionCard from './components/CollectionCard';
+import PunkList from './components/PunkList';
 
 function App() {
   const [punkListData, setPunkListData] = useState([]);
@@ -11,23 +11,20 @@ function App() {
   useEffect(() => {
     const getMyNfts = async () => {
       const openseaData = await axios.get(
-        'https://testnets-api.opensea.io/api/v1/assets?asset_contract_address=0x8Dd973F8b26ef734A36A07be42FBA7AF48D041b1&order_direction=asc'
+        'https://testnets-api.opensea.io/api/v1/assets?asset_contract_address=0x8Dd973F8b26ef734A36A07be42FBA7AF48D041b1&order_direction=desc&offset=0&limit=20&include_orders=false'
       );
       console.log(openseaData.data.assets);
+      setPunkListData(openseaData.data.assets);
     };
 
     getMyNfts();
   }, []);
 
+
   return (
     <div className='app'>
       <Header />
-      <CollectionCard
-        id={0}
-        name={'Bandana Punk'}
-        traits={[{'value': 7}]}
-        image='https://lh3.googleusercontent.com/9B1oTFEv4DeaW9gFwnd1ION2NzfGnC9lHfjnPNSbNkDZmID0P7Di6Y6ptZxZuf0oaWgnAKLu1eqEbdlCJSpHDFUceo2D7H9c0wK56Q=s0'
-      />
+      <PunkList punkListData={punkListData} />
     </div>
     )
 }
